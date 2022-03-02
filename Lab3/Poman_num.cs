24959206abbad2e4f326027d5a61e06803428bdb
T1fon <danil_ignatyev@mail.ru>
2022-03-02 21:15:19 +0700

    Добавьте файлы проекта.

---
+++ Lab3/Poman_num.cs
@@ -0,0 +1,79 @@
+﻿namespace Lab3
+{
+    public class RomanNumber : ICloneable, IComparable
+    {
+        public RomanNumber(ushort n)
+        {
+            int i;
+            if (n == 0)
+                throw new R_N_E("Constructor: n is equal to zero.");
+            if (n >= 4000)
+                throw new R_N_E("Constructor: n more than 3999.");
+            Number = n;
+            RomanString = "";
+            for (i = 0; i < Arr_Size; ++i)
+            {
+                while (n - Values[i] >= 0)
+                {
+                    RomanString += Romans[i];
+                    n -= Values[i];
+                }
+            }
+        }
+        public static RomanNumber operator +(RomanNumber? n1, RomanNumber? n2)
+        {
+            if (n1 == null || n2 == null)
+                throw new R_N_E("Add: at least one object is null.");
+            if (n1.Number + n2.Number >= 4000)
+                throw new R_N_E("Add: sum exceeds 3999.");
+            return new RomanNumber((ushort)(n1.Number + n2.Number));
+        }
+        public static RomanNumber operator -(RomanNumber? n1, RomanNumber? n2)
+        {
+            if (n1 == null || n2 == null)
+                throw new R_N_E("Sub: at least one object is null.");
+            if (n1.Number <= n2.Number)
+                throw new R_N_E("Sub: result is less than or equal to zero.");
+            return new RomanNumber((ushort)(n1.Number - n2.Number));
+        }
+        public static RomanNumber operator *(RomanNumber? n1, RomanNumber? n2)
+        {
+            if (n1 == null || n2 == null)
+                throw new R_N_E("Mul: at least one object is null.");
+            if (n1.Number * n2.Number >= 4000)
+                throw new R_N_E("Mul: result exceeds 3999.");
+            return new RomanNumber((ushort)(n1.Number * n2.Number));
+        }
+        public static RomanNumber operator /(RomanNumber? n1, RomanNumber? n2)
+        {
+            if (n1 == null || n2 == null)
+                throw new R_N_E("Div: at least one object is null.");
+            if (n1.Number / n2.Number == 0)
+                throw new R_N_E("Div: result equals zero.");
+            return new RomanNumber((ushort)(n1.Number / n2.Number));
+        }
+        public override string ToString()
+        {
+            return RomanString;
+        }
+        public object Clone()
+        {
+            return new RomanNumber(Number);
+        }
+        public int CompareTo(object? obj)
+        {
+            if (obj is RomanNumber)
+                return this.Number.CompareTo(((RomanNumber)obj).Number);
+            throw new R_N_E("CompareTo: obj is null or not RomanNumber.");
+        }
+        private static int Arr_Size = 13;
+        private static string[] Romans =
+            { "M", "CM", "D", "CD", "C", "XC", "L",
+            "XL", "X", "IX", "V", "IV", "I" };
+        private static ushort[] Values =
+            {1000, 900, 500,
+            400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        private ushort Number;
+        private string RomanString;
+    }
+}
---
+++ Lab3/R_N_E.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab3
+{
+    public class R_N_E : Exception
+    {
+        public R_N_E() { }
+        public R_N_E(string text) : base(text) { }
+        public R_N_E(string text, R_N_E inner) : base(text, inner) { }
+        protected R_N_E(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
